270ec5e8aaab0720f89bbb04fcf543ae9bbd8931
VorterusHUN <vojsfor@gmail.com>
2025-01-14 10:36:57 +0100

    Add files via upload

---
+++ InSchool/InSchool/Game1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace InSchool
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Sprite sprite;
+        //Texture2D _texture;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            //_texture = Content.Load<Texture2D>("metro");
+            Texture2D texture = Content.Load<Texture2D>("metro");
+            sprite = new Sprite(texture, Vector2.Zero, 1f);
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                sprite.position.X += sprite.speed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                sprite.position.X -= sprite.speed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                sprite.position.Y -= sprite.speed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                sprite.position.Y += sprite.speed;
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            _spriteBatch.Draw(sprite.texture, sprite.position, Color.White);
+            // _spriteBatch.Draw(_texture, new Rectangle(100,100,100,200),Color.White);
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ InSchool/InSchool/Sprite.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InSchool
+{
+    public class Sprite
+    {
+        public float speed;
+        public Texture2D texture;
+        public Vector2 position;
+
+        public Sprite(Texture2D texture, Vector2 position, float speed)
+        {
+            this.texture = texture;
+            this.position = position;
+            this.speed = speed;
+        }
+
+        public Sprite(Texture2D texture, Microsoft.Xna.Framework.Vector2 zero, float speed)
+        {
+            this.texture = texture;
+            this.speed = speed;
+        }
+
+        public void Update()
+        {
+            position.X += speed;
+        }
+    }
+}
